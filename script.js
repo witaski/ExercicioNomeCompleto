fadc56c21f668f406d142466b504b47a92073aac
Wellington Witaski <wellingtonwitaski@hotmail.com>
2022-05-24 10:18:57 -0300

    Exibe o nome completo no formato padrão Nome Sobrenome e no formato de catálogo SOBRENOME, Nome.

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = prompt('digite seu nome:');
+let sobrenome = prompt('digite seu sobrenome:');
+
+alert(`Nome completo: ${nome.toUpperCase()} ${sobrenome}`);
+
+alert(`Nome de catálogo: ${sobrenome.toUpperCase()} ${nome}`);
